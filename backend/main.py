61d19ace9d4581674ac9c20b4dd3811080a34d2b
kripesh101 <33386091+kripesh101@users.noreply.github.com>
2023-04-18 20:53:32 +0545

    backend: registration
    and minor improvements to web

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-
 from routes import web
+from db import init_db
+import cloudinary
 
-# Todo: Initialize database connection here
+init_db()
+
+cloudinary.config(
+  cloud_name = "dgglbr1hh",
+  api_key = "681571449771746",
+  api_secret = "7f7192A6OXTfqO8r14R03ZRp4ks",
+  secure = True
+)
 
 app = FastAPI(root_path="/api")
 
